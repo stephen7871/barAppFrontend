7e1ece0635c74b6ede5282ef0f4d377b319bf2a1
stephen mcnally <stephen.mcn787@gmail.com>
2023-10-22 15:07:02 -0400

    addded this

--- src/App.js
+++ src/App.js
@@ -1,73 +1,64 @@
-// import React,{useState,useEffect} from "react";
-// import classes from './App.module.css'
-// // import "./App.css
-// import Homepage from "./Pages/Homepage";
-// import {BrowserRouter, Routes, Route } from "react-router-dom";
-// import Chatpage from "./Pages/Chatpage";
-// import Home from "./components/Home/Home";
-// import FirstPage from "./components/meetups/firstpage/FirstPage";
+import { React, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from "./components/add-review";
+import Login from "./components/login";
+import Restaurant from "./components/restaurant";
+import RestaurantsList from "./components/restaurants-list";
 
-
-// function App() {
-
-
-//   const [currentId, setCurrentId] = useState(0);
-
-  
-
-//   return (
-//     <div className={classes.App}>
-//       <Routes>
-//         {/* <Route path="/" element={<Homepage />}/> */}
-//         <Route path="/" element={<FirstPage />}/>
-//       {/* <Route path="/" component={Homepage} exact /> */}
-//       {/* <Route path="/chats" element={<Chatpage />} /> */}
-//       <Route path="/Home/*" element={<Home currentId={currentId} setCurrentId={setCurrentId} />}/>   
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React,{useState,useEffect} from "react";
-import "./App.css";
-
-import {BrowserRouter, Routes, Route } from "react-router-dom";
-import MyPost from "./components/postComponents/MyPost";
-import PostList from "./components/postComponents/PostList";
-import NewPost from "./components/postComponents/NewPost";
-import Home from "./components/Home/Home";
-import Homepage from "./Pages/Homepage";
-import Navbar from "./components/Navbar/Navbar";
 function App() {
+  const [user, setUser] = useState(null);
 
-  const [username, setUserName] = useState("");
-  useEffect(async () => {
-    setUserName(
-      await JSON.parse(localStorage.getItem("userInfo")
-      )
-    );
-  }, []);
+  async function login(user = null) {
+    setUser(user);
+  }
 
-  
-  const [currentId, setCurrentId] = useState(0);
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div className="App">
-            <Routes>
-        {/* <Route path="/" element={<Homepage />}/> */}
-      
-        <Route path="/*" element={<Home currentId={currentId} setCurrentId={setCurrentId} />}/>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand ms-3">
+          Restaurant Reviews
+        </a>
+        <div className="navbar-nav me-auto">
+          <li className="nav-item">
+            <Link to="/restaurants" className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item">
+            {user ? (
+              <a onClick={logout} className="nav-link">
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to="/login" className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
 
-
-        {/* <Route path="/Auth" element={<Homepage />}/>  
-             <Route path="/Blog" element={<PostList currentId={currentId} setCurrentId={setCurrentId} user={username} setUser={setUserName}/>}/> 
-            
-            <Route path="/NewPost" element={<NewPost currentId={currentId} setCurrentId={setCurrentId} user={username} setUser={setUserName}/>}/>
-           
-            <Route path="/MyPost" element={<MyPost currentId={currentId} setCurrentId={setCurrentId} user={username} setUser={setUserName}/>}/>  */}
-      {/* <Route path="/chats" element={<Chatpage />} /> */}
-      {/* <Route path="/Home/*" element={<Home currentId={currentId} setCurrentId={setCurrentId} />}/>    */}
-      </Routes>
+      <div className="conatiner mt-3">
+        <Routes>
+          <Route exact path="/restaurants" element={<RestaurantsList />} />
+          <Route
+            exact
+            path="/restaurants/:id/review"
+            element={(props) => <AddReview {...props} user={user} />}
+          />
+          <Route
+            exact
+            path="/restaurants/:id"
+            element={(props) => <Restaurant {...props} user={user} />}
+          />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
